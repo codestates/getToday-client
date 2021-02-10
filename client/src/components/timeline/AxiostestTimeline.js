74a58bb87353336240db3e58b54d14bcd66975b3
“yoonkm24” <yoonkm0424@gmail.com>
2021-02-10 21:36:41 +0900

    revise

--- client/src/components/timeline/AxiostestTimeline.js
+++ client/src/components/timeline/AxiostestTimeline.js
@@ -1,47 +1,49 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+// import React, { Component } from 'react';
+// import axios from 'axios';
 
-const https = require('https');
+// const https = require('https');
 
-class AxiosTest extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      message : ''
-    };
-    this.getMessage = this.getMessage.bind(this);
-  }
-  getMessage () {
-    axios
-    //https://jsonplaceholder.typicode.com/posts
-    //https://13.125.233.189:4000
-    //https://www.gettoday3.tk
-      .get("https://jsonplaceholder.typicode.com/posts",{
-        withCredentials: true,
+// class AxiosTest extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       posts: [],
+//       message : ''
+//     };
+//     this.getMessage = this.getMessage.bind(this);
+//   }
+//   getMessage () {
+//     axios
+//     //https://jsonplaceholder.typicode.com/posts
+//     //https://13.125.233.189:4000
+//     //https://www.gettoday3.tk
+//       .get("https://jsonplaceholder.typicode.com/posts",{
+//         withCredentials: true,
         
-        httpsAgent: new https.Agent({
-          rejectUnauthorized: false
-        })
-      })
-      .then((res)=>{
-        console.log('test')
-          console.log(res.data.slice(0,10))
-        this.setState({posts : res.data.slice(0,10)})
-//        this.setState({message : res.data})
+//         httpsAgent: new https.Agent({
+//           rejectUnauthorized: false
+//         })
+//       })
+//       .then((res)=>{
+//         console.log('test')
+//           console.log(res.data.slice(0,10))
+//         this.setState({posts : res.data.slice(0,10)})
+// //        this.setState({message : res.data})
 
-      });
-  }
+//       });
+//   }
 
-  componentDidMount() {
-      this.getMessage();
-  }
+//   componentDidMount() {
+//       this.getMessage();
+//   }
 
-  render () { 
-    return(
-      <div>test message : {this.state.message}</div>
-    )  
-  }  
-}
+//   render () { 
+//     return(
+//       <div>test message : {this.state.message}</div>
+//     )  
+//   }  
+// }
 
-export default AxiosTest;
+// export default AxiosTest;
+
+// //AxiostestTimeline(model) + mypage - datatimeline(controller) - 가공해서 viewTimeline
--- client/src/components/todo/todocollection.js
+++ client/src/components/todo/todocollection.js
@@ -1,23 +1,23 @@
-import React from 'react';
+// import React from 'react';
 
-const Todos = ({todos, deleteTodo}) => {
-    const todoList = todos.length ? (
-        todos.map( todo => {
-            return (
-                <div className = 'collectionItem' key={todo.id}>
-                    <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
-                </div>
-            )
-        })
-    ) : (
-        <div>todo is empty</div>
-    );
+// const Todos = ({todos, deleteTodo}) => {
+//     const todoList = todos.length ? (
+//         todos.map( todo => {
+//             return (
+//                 <div className = 'collectionItem' key={todo.id}>
+//                     <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+//                 </div>
+//             )
+//         })
+//     ) : (
+//         <div>todo is empty</div>
+//     );
     
-    return (
-        <div className = 'todoscollection'>
-            {todoList}
-        </div>
-    )
-}
+//     return (
+//         <div className = 'todoscollection'>
+//             {todoList}
+//         </div>
+//     )
+// }
 
-export default Todos
+// export default Todos
